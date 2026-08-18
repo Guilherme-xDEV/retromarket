2c6c943094d381e5748be1413a8941bfa8e2cd0d
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-18 16:35:37 -0300

    feat: implemented method to list all adds

--- src/main/java/br/edu/ifce/retromarket/services/AnuncioService.java
+++ src/main/java/br/edu/ifce/retromarket/services/AnuncioService.java
@@ -1,5 +1,20 @@
 package br.edu.ifce.retromarket.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifce.retromarket.entities.Anuncio;
+import br.edu.ifce.retromarket.repositories.AnuncioRepository;
+
+@Service
 public class AnuncioService {
     
+    @Autowired
+    private AnuncioRepository anuncioRepository;
+
+    public List<Anuncio> listarAnuncios() {
+        return anuncioRepository.findAll();
+    }
 }
