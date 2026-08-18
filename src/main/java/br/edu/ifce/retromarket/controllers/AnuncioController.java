1bd6e52498d983fe8e5b11802293358c9ec9d726
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-18 17:02:05 -0300

    refactor: implemented AnuncioService and refactored AnuncioController

--- src/main/java/br/edu/ifce/retromarket/controllers/AnuncioController.java
+++ src/main/java/br/edu/ifce/retromarket/controllers/AnuncioController.java
@@ -1,5 +1,6 @@
 package br.edu.ifce.retromarket.controllers;
 
+import br.edu.ifce.retromarket.services.AnuncioService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,23 +10,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ifce.retromarket.entities.Anuncio;
 import br.edu.ifce.retromarket.entities.Completude;
-import br.edu.ifce.retromarket.repositories.CompletudeRepository;
 
 @RestController
-@RequestMapping(value = "/completudes")
+@RequestMapping(value = "/anuncios")
 public class AnuncioController {
 
   @Autowired
-  private CompletudeRepository repository;
+  private AnuncioService service;
 
   @GetMapping("/completudes")
   public List<Completude> buscarCompletudes() {
-    return repository.findAll();
+    return service.buscarCompletudes();
   }
 
   @PostMapping("/completudes")
   public Completude criarCompletude(@RequestBody Completude completude) {
-    return repository.save(completude);
+    return service.criarCompletude(completude);
+  }
+
+  @GetMapping
+  public List<Anuncio> listarAnuncios() {
+    return service.listarAnuncios();
   }
 }
