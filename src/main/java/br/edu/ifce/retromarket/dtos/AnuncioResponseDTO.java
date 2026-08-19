7e63889d6163372d6a7ac1004a4f93fa9e0adead
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-18 22:58:28 -0300

    feat: DTO class implementation for Anuncio

---
+++ src/main/java/br/edu/ifce/retromarket/dtos/AnuncioResponseDTO.java
@@ -0,0 +1,14 @@
+package br.edu.ifce.retromarket.dtos;
+
+public class AnuncioResponseDTO {
+    private String titulo;
+    private String preco;
+    private String condicao;
+    private String completude;
+    private String status;
+    private String localizacao;
+    private String urlFotoPrincipal;
+    private PlataformaResumoDTO plataforma;
+    private CategoriaResumoDTO categoria;
+
+}
